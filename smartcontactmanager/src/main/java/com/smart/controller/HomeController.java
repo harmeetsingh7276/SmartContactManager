ef0e982db06c6b22cd4aa6e8d69ae4588e55847b
harmeet <harmeetsingh7276@gmail.com>
2022-08-22 20:02:37 +0530

    Adding base page for the web application and injecting the base page for home and about page, Also making file structure for user defined custom css and js

--- smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
@@ -1,29 +1,21 @@
 package com.smart.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.smart.dao.UserRepository;
-import com.smart.entities.Contact;
-import com.smart.entities.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
-	@Autowired
-	private UserRepository userRepository;
+	@RequestMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("title","Home - Smart Contact Manager");
+		return "home";
+	}
 	
-	@GetMapping("/test")
-	@ResponseBody
-	public String test() {
-		User user=new User();
-		user.setName("Harmeet");
-		user.setEmail("harmeetsingh7276@gmail.com");
-		Contact contact=new Contact();
-		user.getContacts().add(contact);
-		userRepository.save(user);
-		return "Working";
+	@RequestMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title","About - Smart Contact Manager");
+		return "about";
 	}
 }
